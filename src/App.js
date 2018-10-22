1f71482d6c8c2521764c708e3f4f5707f0a59124
Lexi Ross <lexi@aspen.team>
2018-10-21 19:41:03 -0700

    rotate between chat topics when current topic is finished

--- src/App.js
+++ src/App.js
@@ -17,29 +17,50 @@ const DAY_OF_WEEK = {
     THURSDAY: 4,
     FRIDAY: 5,
     SATURDAY: 6,
-    SUNDAY: 7,
 };
 
 const CHAT_TOPICS = {
     FURRIES: 'furries',
+    POLY: 'poly',
     // TODO finish me
 };
 
 const CHATS_BY_TOPIC_AND_DAY = {
     [CHAT_TOPICS.FURRIES]: {
-        [DAY_OF_WEEK.THURSDAY]: [], // TODO fill in with imported data
+        // TODO fill in with imported data
+        [DAY_OF_WEEK.THURSDAY]: [{message: 'I LOVE CURRY'}, {message: 'yes we do'}],
+        [DAY_OF_WEEK.FRIDAY]: [],
+        [DAY_OF_WEEK.SATURDAY]: [],
         // etc..
+    },
+    [CHAT_TOPICS.POLY]: {
+        // TODO fill in with imported data
+        [DAY_OF_WEEK.THURSDAY]: [{message: 'poly is lots of fun'}, {message: 'yes it is'}, {message: 'my nam beryl'}],
+        [DAY_OF_WEEK.FRIDAY]: [],
+        [DAY_OF_WEEK.SATURDAY]: [],
     },
     // etc...
 };
 
-const MIN_DURATION = 300;
+const MIN_DURATION = 500;
 const DURATION_PER_CHAR = 10;
 
-// TODO snap to a day that's thursday-sunday
-const dayOfWeek = qs.parse(window.location.search).day || moment().day();
+// TODO account for the fact that each day at RS includes the next day's early morning
+const getDayOfWeek = (): number => {
+    const dayFromURL = qs.parse(window.location.search).day;
+    if (dayFromURL != null) {
+        return dayFromURL;
+    }
+    const currentDay = moment().day();
+    if (currentDay === DAY_OF_WEEK.THURSDAY || currentDay === DAY_OF_WEEK.FRIDAY || currentDay === DAY_OF_WEEK.SATURDAY) {
+        return currentDay;
+    }
+    return DAY_OF_WEEK.THURSDAY;
+}
+const dayOfWeek = getDayOfWeek();
 const chatTopic = qs.parse(window.location.search).topic;
 
+// finds the correct list of messages to display based on the query string
 const getChatsToDisplay = (): Array<Chat> => {
     if (!chatTopic) {
         return defaultChats;
@@ -50,7 +71,7 @@ const getChatsToDisplay = (): Array<Chat> => {
         return defaultChats;
     }
 
-    const chatsForDay = CHATS_BY_TOPIC_AND_DAY[dayOfWeek];
+    const chatsForDay = chatsForChatTopic[dayOfWeek];
     return chatsForDay || defaultChats;
 };
 
@@ -71,10 +92,28 @@ export default class App extends React.Component<{}, State> {
     loadNextChat = () => {
         const { nextChatIndex } = this.state;
         if (nextChatIndex >= allChats.length) {
-            return;
+            // if we're out of messages, refresh the page with a new topic
+            return this.loadNextTopic();
         }
+        // otherwise, increment the message index and set a timeout
+        // based on the length of the new message
         setTimeout(this.loadNextChat, getDurationForChat(allChats[nextChatIndex]));
         this.setState({...this.state, nextChatIndex: nextChatIndex+1});
+    }
+
+    loadNextTopic = () => {
+        const allTopics = Object.keys(CHAT_TOPICS).map((key) => CHAT_TOPICS[key]).filter((topic) => (
+            CHATS_BY_TOPIC_AND_DAY[topic][dayOfWeek].length > 0
+        ));
+        let nextTopic = allTopics[0];
+        const currentTopic = chatTopic;
+        for (let i = 0; i < allTopics.length; i++) {
+            if (allTopics[i] === currentTopic) {
+                nextTopic = allTopics[(i + 1) % allTopics.length];
+            }
+        }
+
+        window.location.search = qs.stringify({ day: dayOfWeek, topic: nextTopic });
     }
 
     render() {
